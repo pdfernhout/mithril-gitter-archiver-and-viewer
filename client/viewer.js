8e6f0e34af55347a8a02438d8d4e44a34c55edc6
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-01-31 23:05:51 -0500

    improve viewer to have sorted table that can display posts for a selected user

--- client/viewer.js
+++ client/viewer.js
@@ -5,6 +5,128 @@ let stats_users = "Loading..."
 let users = null
 let messages = null
 
+let currentUsername = null
+
+/* Example message:
+  "id": "550198834c839f3268162e60",
+  "sent": "2015-03-12T13:45:39.963Z",
+  "username": "StephanHoyer",
+  "text": "Hi Leo"
+ }
+ */
+/* example user:
+{
+ "spacejack": {
+  "id": "55edda3f0fc9f982beafee8a",
+  "username": "spacejack",
+  "displayName": "spacejack",
+  "url": "/spacejack",
+  "avatarUrl": "https://avatars-01.gitter.im/gh/uv/4/spacejack",
+  "avatarUrlSmall": "https://avatars1.githubusercontent.com/u/11592867?v=4&s=60",
+  "avatarUrlMedium": "https://avatars1.githubusercontent.com/u/11592867?v=4&s=128",
+  "v": 18,
+  "gv": "4"
+}
+*/
+
+function displayMessagesForUser(username) {
+    const result = []
+    for (const message of messages) {
+        if (message.username === username) {
+            result.push(message)
+        }
+    }
+    return result.map(message => m("div.ml3", 
+        { key: message.id }, 
+        m("span.blue", message.sent),
+        " ", 
+        m("span", /* { style: "white-space: pre" }, */ message.text)
+    ))
+}
+
+let sortBy = "id"
+let sortReverse = false
+
+function headerClick(field) {
+    currentUsername = null
+    if (sortBy === field) {
+        sortReverse = !sortReverse
+    } else {
+        sortBy = field
+    }
+}
+
+function displayViewer() {
+    const sortedUsers = Object.keys(users)
+    switch (sortBy) {
+    case "id":
+        sortedUsers.sort()
+        break
+    case "name":
+        sortedUsers.sort((a, b) => users[a].displayName.localeCompare(users[b].displayName))
+        break
+    case "rank":
+        sortedUsers.sort((a, b) => users[b].rank - users[a].rank)
+        break
+    case "posts":
+        sortedUsers.sort((a, b) => users[b].postCount - users[a].postCount)
+        break
+    default:
+        throw new Error("unexpected sort case")
+    }
+    if (sortReverse) sortedUsers.reverse()
+    const table = sortedUsers.map(username => {
+        const user = users[username]
+        const isSelected = currentUsername === username
+        return m("div.ml2", { key: user.id },
+            m("span" + (isSelected ? ".b" : ""), 
+                {
+                    onclick: () => currentUsername === username ? currentUsername = null : currentUsername = username
+                },
+                m("span.dib", { style: "width: 12rem" }, user.username),
+                " ",
+                m("span.i.dib", { style: "width: 12rem" }, user.displayName),
+                " ",
+                m("span.dib.w3", user.rank),
+                " ",
+                m("span.dib.w3", user.postCount)
+            ),
+            isSelected ? displayMessagesForUser(username) : []
+        )
+    })
+    const sortCharacter = m("span.b", sortReverse ? "▲" : "▼")
+    const header = m("div.ml2", { key: " HEADER " },
+        m("span", 
+            m("span.dib", { style: "width: 12rem", onclick: () => headerClick("id") }, "ID", ((sortBy === "id") ? sortCharacter : [])),
+            " ",
+            m("span.dib", { style: "width: 12rem", onclick: () => headerClick("name") }, "Name", ((sortBy === "name") ? sortCharacter : [])),
+            " ",
+            m("span.dib.w3", { onclick: () => headerClick("rank") }, "Rank", ((sortBy === "rank") ? sortCharacter : [])),
+            " ",
+            m("span.dib.w3", { onclick: () => headerClick("posts") }, "# Posts", ((sortBy === "posts") ? sortCharacter : []))
+        )
+    )
+    return [header, table]
+}
+
+const GitterArchiveViewer = {
+    view: function() {
+        const isEverythingLoaded = stats_messages && messages && stats_users && users
+        return m(".main", [
+            m("h1.ba.b--blue", { class: "title" }, "Gitter Archive Viewer"),
+            m("div", "messages count: ", messages ? messages.length : m("span.yellow", "Loading...")),
+            m("br"),
+            isEverythingLoaded 
+                ? [ displayViewer() ] 
+                : [
+                    m("div", "stats_messages: ", stats_messages ? stats_messages : m("span.yellow", "Loading...")),
+                    m("div", "stats_users: ", stats_users ? "Loaded" : m("span.yellow", "Loading...")),
+                    m("div", "users: ", users ? "Loaded" : m("span.yellow", "Loading..."))
+                ]
+        ])
+    }
+}
+
 async function startup() {
     stats_messages = await m.request({
         method: "GET",
@@ -23,22 +145,28 @@ async function startup() {
         url: "/data/allUsers.json"
     })
 
-    messages = await m.request({
+    // await on this later so we can process the user data while we are waiting
+    const promiseForAllMessages = m.request({
         method: "GET",
         url: "/data/allMessages.json"
     })
+
+    updateUserRankAndPostCount()
+
+    messages = await promiseForAllMessages
 }
 
-const GitterArchiveViewer = {
-    view: function() {
-        return m(".main", [
-            m("h1.ba.b--blue", { class: "title" }, "Gitter Archive Viewer"),
-            m("div", "stats_messages: ", stats_messages ? stats_messages : m("span.yellow", "Loading...")),
-            m("div", "messages count: ", messages ? messages.length : m("span.yellow", "Loading...")),
-            m("div", "stats_users: ", stats_users ? "Loaded" : m("span.yellow", "Loading...")), // m("pre", stats_users)
-            m("div", "users: ", users ? "Loaded" : m("span.yellow", "Loading...")),
-        ])
-    }
+function updateUserRankAndPostCount() {
+    stats_users.split("\n").forEach(line => {
+        if (!line.trim()) return
+        const [ rank, username, postCount ] = line.split(" ")
+        const user = users[username]
+        if (!user) {
+            throw new Error("missing user for: " + username)
+        }
+        user.rank = rank
+        user.postCount = postCount
+    })
 }
 
 m.mount(document.body, GitterArchiveViewer)
